c47ece9b6ddd2a6e493cb2e605d262cd0cda6e50
teejay009 <tajdjimoh@gmail.com>
2023-11-09 11:49:37 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,32 @@ import React from "react";
 
 
   
-  return ['Muhammad', "Fine Boy"]
-}
+  
+
 
 const App = () => {
-  const [userInput, setUserInput] = useState("");
   const [randomInput, setRandomInput] = useState("");
 
-  const fib = useCallback((n) => {
-    return n <= 1 ? n : fib(n-1) + fib(n-2)
 
 
-  }, [])
+  
   
 
 
 
-  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
+  
 
-  const myArray = useMemo(() => getArray(), [])
-  useEffect(() => {
-    console.log("New Array");
-  }, [myArray])
+  
 
 
 
   return(
     <main className="App">
-      <label htmlFor="">Fibonacci Sequence</label>
-      <input type="text" placeholder="position" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+     
 
-      <p>Number: {fibNumber || "--"}</p>
-      <br />
-      <br />
-
-      <label htmlFor="">Random Input:</label>
-      <input type="text" 
+      
+      <input
+       type="text" 
       value={randomInput}
       placeholder='Type anything...'
       onChange={(e) => setRandomInput(e.target.value)}
