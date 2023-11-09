033d4ce848fad88ac3115843bea73b499cd17e0a
teejay009 <tajdjimoh@gmail.com>
2023-11-09 11:51:02 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -26,16 +26,21 @@ const App = () => {
 
   return(
     <main className="App">
-     
-
-      
-      <input
+        
+        <input
        type="text" 
       value={randomInput}
       placeholder='Type anything...'
       onChange={(e) => setRandomInput(e.target.value)}
       />
       <p>Result: {randomInput}</p>
+
+      <br />
+      <br />
+
+      <section>
+        
+      </section>
 
     </main>
 
